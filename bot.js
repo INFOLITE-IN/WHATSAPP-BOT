00be4d513a6560acecf36ffc259767be920c389d
SOCIAL MECHANIC <95063433+SOCIAL-MECHANIC-1997@users.noreply.github.com>
2022-04-11 15:43:20 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2022 social-mechanic-1997.
+/* Copyright (C) 2020 social-mechanic-1997.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
